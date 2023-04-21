f1f4ac597073faeac3902b928bdc86ce25adf786
John Irvin Panganiban <jipanganiban@gbox.adnu.edu.ph>
2023-04-21 20:06:42 +0800

    4th commit panganiban

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.navbar')
 
 @section('content')
-    
+
+<body div class="bg-img"></div></body>
+<!-- <body style="background: url('../assets/images/homebckgrnd.jpg'); background-repeat: no-repeat; background-size: 100%;"> -->
+    <img class="home-logo" src="assets/images/homelogo.png" alt="home logo">
+    <hr>
+    <h1>Safe and Reliable Travel Experience</h1><hr>
+    <h2>Going the extra mile for you</h2>
+    <h4>TricyCALL application will work by allowing tricycle operators to register and list their services on the platform. Customers can then search for available tricycles using the application, view tricycle operators' profiles and reviews, and make an informed decision when selecting a tricycle.
+</h4>
+</body>
+        
 @endsection
